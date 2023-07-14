8710c4554c68618beb7ebff11a07f678bfe39eab
deepfelt <dgodesign77@gmail.com>
2023-07-14 18:24:12 -0400

    modified:   src/components/World3d.tsx

--- src/components/World3d.tsx
+++ src/components/World3d.tsx
@@ -1,4 +1,4 @@
-import { PerspectiveCamera, Text } from "@react-three/drei";
+import {Scroll, Image, MeshReflectorMaterial, PerspectiveCamera, Text, TorusKnot } from "@react-three/drei";
 export const World3d=()=>{
     
     
@@ -9,7 +9,13 @@ export const World3d=()=>{
         <>
         
 <PerspectiveCamera/>
+<Scroll>
+<Image url="/wallpapers.png" position={[0,0,-4]} scale={[32,18]}/>
+</Scroll>
 <Text>This is NOT a DOM element</Text>
+<TorusKnot position={[0,2,-2]}>
+<MeshReflectorMaterial mirror={0} />
+</TorusKnot>
 </>
     )}
     
