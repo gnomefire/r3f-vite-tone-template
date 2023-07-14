7ccc44d07ddc02e62140a116ed6d3a61aa31bfad
deepfelt <dgodesign77@gmail.com>
2023-07-14 19:34:36 -0400

    modified:   src/components/World3d.tsx

--- src/components/World3d.tsx
+++ src/components/World3d.tsx
@@ -1,20 +1,31 @@
-import {Scroll, Image, MeshReflectorMaterial, PerspectiveCamera, Text, TorusKnot } from "@react-three/drei";
+import {PointLight, useScroll, Scroll, Image, MeshReflectorMaterial, PerspectiveCamera, Text, TorusKnot } from "@react-three/drei";
+import { useFrame } from "@react-three/fiber";
+import {useRef} from 'react'
+import { MeshStandardMaterial } from "three";
 export const World3d=()=>{
-    
-    
+    const scroll = useScroll()
+    const image1 = useRef()
+    const knot = useRef()
+    useFrame((state)=>{
+        knot.current.rotation.y = ( scroll.offset * 10)
+    })
         
       
    
     return(
         <>
-        
+
 <PerspectiveCamera/>
+<Text >This is kNOT</Text>
 <Scroll>
-<Image url="/wallpapers.png" position={[0,0,-4]} scale={[32,18]}/>
+    <group >
+<Image ref={image1}  url="/wallpapers.png" position={[0,0,-4]} />
+</group>
 </Scroll>
-<Text>This is NOT a DOM element</Text>
-<TorusKnot position={[0,2,-2]}>
-<MeshReflectorMaterial mirror={0} />
+
+<TorusKnot position={[0,2,-2]} ref={knot}>
+<meshStandardMaterial color="violet"/>
+    <pointLight />
 </TorusKnot>
 </>
     )}
